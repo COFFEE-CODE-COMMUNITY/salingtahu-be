c2012ddb4dd1112943fedc14b9a284ba3fdeecee
MuhammadFauzi27 <fauzym02@gmail.com>
2025-10-07 10:09:36 +0700

    feat(auth): add DTOs for authentication processes

---
+++ src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger"
+
+export class LoginDto {
+  @ApiProperty({
+    description: "Email address of the user.",
+    example: "user@example.com",
+    format: "email",
+    required: true,
+    minLength: 1,
+    maxLength: 100,
+  })
+  public email!: string
+
+  @ApiProperty({
+    description: "Password of the user.",
+    example: "P@ssw0rd!",
+    required: false,
+    minLength: 1,
+    maxLength: 100,
+  })
+  public password?: string
+
+}
