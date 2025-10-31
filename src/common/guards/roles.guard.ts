846cdd1bc3c252e9cf4139129f20f199d285294e
Muhammad Taufiqurrahman <mhmtaufiqurrahman@gmail.com>
2025-10-31 11:12:35 +0700

    fix: fix roles guard bug

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -24,7 +24,8 @@ export class RolesGuard implements CanActivate {
     const request = context.switchToHttp().getRequest<Request>()
     const userId = request.userId
 
-    if (!request.authorized) {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (!request.authorized || !roles) {
       return true
     }
 
